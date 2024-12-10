159bee928e7969a17f38d2bd370c7ba6781be7a0
tarasowski <dimitri+github@tarasowski.de>
2024-12-10 08:33:04 +0100

    feat: added hotreload

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ build:
 	GOOS=linux GOARCH=amd64 go build -o ./tmp/server ./cmd/server/main.go
 	scp ./public/index.html mvpfoundry:/home/ubuntu/public/index.html
 	scp ./tmp/server mvpfoundry:/home/ubuntu/
+
+dev:
+	npx vite public
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,21 +5,34 @@ import (
 	"net/http"
 )
 
-
 func main() {
+	// focus on cto, cio, architect, tech lead, senior dev, dev
+	// golang + ai
+	// teaching + courses
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", handleRoot)
-
-	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Health check")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	mux.HandleFunc("GET /ai", handleGoLangAI)
+	mux.HandleFunc("GET /health", handleHealth)
 
 
 	http.ListenAndServe(":8080", mux)
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Health check")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func handleGoLangAI(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Welcome to Golang AI")
+	fmt.Println("AI Golang Libraries / Content and more")
+	// this is an endpoint everything around golang + ai
+	// list of libraries, projects, articles, etc
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
